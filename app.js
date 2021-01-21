280f607a74ec66ec6c3a7447f549018a7beaf1ca
Ankit-Nag <ankitknag@gmail.com>
2021-01-21 23:58:45 +0530

    wired toggle to JS
    feat: JS DOM to dark mode toggle

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnToggle = document.querySelector("#toggleSwitch");
+// var btn = document.querySelector("#test")
+
+function output() {
+
+}
+
+
+
+btnToggle.addEventListener("click", output);
