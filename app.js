2eb8f6a029003c690f69dcd9b4dcb1fe6f99c7a0
Ankit-Nag <ankitknag@gmail.com>
2021-01-22 23:30:09 +0530

    wired up JS to input fields
    feat: input values wired to JS

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var btnToggle = document.querySelector("#toggleSwitch");
 var theme = document.querySelector("#theme_link");
+var borderRadius = document.querySelector("#border-radius");
+var borderThickness = document.querySelector("#border-thickness");
+var borderColour = document.querySelector("#border-colour");
 const currentTheme = localStorage.getItem("theme");
+
 var userTheme;
-// var btn = document.querySelector("#test")
+
+
 if(currentTheme =="dark" ){
     theme.href = "/dark-theme.css";
     btnToggle.checked=true;
@@ -23,6 +28,20 @@ function toggleTheme() {
     localStorage.setItem("theme",userTheme);
 }
 
+function adjustRadius(){
+    console.log(borderRadius.value);
+    
+}
 
+function changeColour(){
+    console.log(borderColour.value);
+}
 
+function adjustThickness(){
+    console.log(borderThickness.value);
+}
+
+borderRadius.addEventListener("input",adjustRadius);
+borderColour.addEventListener("input",changeColour);
+borderThickness.addEventListener("input",adjustThickness);
 btnToggle.addEventListener("click", toggleTheme);
